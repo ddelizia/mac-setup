e0ca8c5d68011f84ac0324314870e910db93f893
Danilo Delizia <ddelizia@gmail.com>
2021-01-26 11:18:18 +0100

    Update full_install.sh

--- setup/full_install.sh
+++ setup/full_install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # SdkMan
 curl -s "https://get.sdkman.io" | bash
 sdk install java
 
-# Install brew packages
-xargs brew install < data/brew.data
-xargs brew cask install < data/brew-cask.data
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle innstall
 
 # Visual studio code
 xargs code --install-extension < data/code.data
